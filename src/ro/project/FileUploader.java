748a0f321f9994ba984a25d278d05b3046eaf6f8
Alessio <alessio@manai.tk>
2018-07-02 18:52:52 +0200

    spaghetti code

--- src/ro/project/FileUploader.java
+++ src/ro/project/FileUploader.java
@@ -1,6 +1,7 @@
 package ro.project;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class FileUploader {
 
@@ -9,6 +10,7 @@ public class FileUploader {
     private int capacitaVeicolo;
     private NodoDeposito nodoDeposito;
     private BufferedReader file;
+    private String currentLine;
 
     public FileUploader() {
         this.numeroClienti = 0;
@@ -19,12 +21,41 @@ public class FileUploader {
     }
 
     public void caricaIstanza(String file_path) {
+
         try {
+
+            int x, y, delivery, pickup = 0;
 
             file = new BufferedReader(
                     new FileReader("project_files/Instances/" + file_path + ".txt")
             );
 
+            numeroClienti = Integer.parseInt(file.readLine());
+            file.readLine(); //scarto la seconda riga, sempre 1
+            numeroVeicoli = Integer.parseInt(file.readLine());
+
+            currentLine = file.readLine();  //quarta riga
+
+            String[] dati = currentLine.split("   ");
+
+            x = Integer.parseInt(dati[0]);
+            y = Integer.parseInt(dati[1]);
+            delivery = Integer.parseInt(dati[2]);
+
+            nodoDeposito = new NodoDeposito(x, y, delivery);
+
+            while ((currentLine = file.readLine()) != null) {
+
+                dati = currentLine.split("   ");
+
+                x = Integer.parseInt(dati[0]);
+                y = Integer.parseInt(dati[1]);
+                delivery = Integer.parseInt(dati[2]);
+                pickup = Integer.parseInt(dati[3]);
+
+                System.out.println(x + " " + y + " " + delivery + " " + pickup);
+
+            }
 
         } catch (Exception e) {
 
