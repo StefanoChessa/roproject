35cf65268f95717961e1f052aad498587b99c047
Alessio <alessio@manai.tk>
2018-07-02 18:12:22 +0200

    added scanner

--- src/ro/project/Main.java
+++ src/ro/project/Main.java
@@ -1,10 +1,19 @@
 package ro.project;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Ciao");
         FileUploader file = new FileUploader();
+
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.println("Inserisci il nome del file:");
+        name = scanner.nextLine();
+
+        System.out.println(name);
     }
 }
