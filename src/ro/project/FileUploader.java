360c1ab6fb43f467e85252401ea038865a11340a
StefanoChessa <sr.chessa@tiscali.it>
2018-07-02 18:22:24 +0200

    added read from file method

--- src/ro/project/FileUploader.java
+++ src/ro/project/FileUploader.java
@@ -8,12 +8,27 @@ public class FileUploader {
     private int numeroVeicoli;
     private int capacitaVeicolo;
     private NodoDeposito nodoDeposito;
+    private BufferedReader buffer;
 
     public FileUploader() {
         this.numeroClienti = 0;
         this.numeroVeicoli = 0;
         this.capacitaVeicolo = 0;
         this.nodoDeposito = null;
+        this.buffer = null;
     }
 
+    public void caricaIstanza(String file) {
+        try {
+
+            buffer = new BufferedReader(
+                    new FileReader("project_files/Instances/" + file + ".txt")
+            );
+            
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
 }
--- src/ro/project/Main.java
+++ src/ro/project/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("Ciao");
+
+        String filename;
         FileUploader file = new FileUploader();
-
         Scanner scanner = new Scanner(System.in);
-        String name;
-        System.out.println("Inserisci il nome del file:");
-        name = scanner.nextLine();
 
-        System.out.println(name);
+        System.out.println("Inserisci il nome del file da importare (non includere il .txt):");
+        filename = scanner.nextLine();
+
+        file.caricaIstanza(filename);
+
     }
 }
