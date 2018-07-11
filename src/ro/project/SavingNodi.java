38886d4763cd46ee004fbd4bb04af7987a20d9bf
Alessio <alessio@manai.tk>
2018-07-11 15:49:18 +0200

    classe saving dei nodi

---
+++ src/ro/project/SavingNodi.java
@@ -0,0 +1,41 @@
+package ro.project;
+
+public class SavingNodi {
+
+    private Nodo nodoA;
+    private Nodo nodoB;
+    private Double saving;
+
+    public SavingNodi(NodoCliente a, NodoDeposito b, Double s){
+
+        this.setNodoA(a);
+        this.setNodoB(b);
+        this.setSaving(s);
+
+    }
+
+
+    public Nodo getNodoA() {
+        return nodoA;
+    }
+
+    public void setNodoA(Nodo nodoA) {
+        this.nodoA = nodoA;
+    }
+
+    public Nodo getNodoB() {
+        return nodoB;
+    }
+
+    public void setNodoB(Nodo nodoB) {
+        this.nodoB = nodoB;
+    }
+
+    public Double getSaving() {
+        return saving;
+    }
+
+    public void setSaving(Double saving) {
+        this.saving = saving;
+    }
+}
