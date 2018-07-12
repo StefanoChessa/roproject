159f4482c1ab32f345bf4d311f79f387cbbaf7bf
StefanoChessa <sr.chessa@tiscali.it>
2018-07-12 16:42:56 +0200

    Rimossa Main Rotta

--- src/ro/project/Rotta.java
+++ src/ro/project/Rotta.java
@@ -6,30 +6,28 @@ import java.util.ArrayList;
 /**
  * Created by marco on 10/07/2018.
  */
-public class Rotta extends MainRotta {
+public class Rotta {
 
     private int indiceVeicolo;
     private ArrayList<Nodo> nodi;
-    private Double costo= 0.0;
+    private Double costo = 0.0;
 
 
-    public Rotta(int indice){
-        this.indiceVeicolo=indice;
-        nodi=new ArrayList<>();
+    public Rotta(int indice) {
+        this.indiceVeicolo = indice;
+        nodi = new ArrayList<>();
     }
 
-    public void aggiungiAllaRotta(Nodo n){
-
-        if(nodi.size()>0) {
-             this.costo += MatriceDistanze.getInstanza().getDistanza(n.getId(),nodi.get(nodi.size()-1).getId());
+    public void aggiungiAllaRotta(Nodo n) {
+        if (nodi.size() > 0) {
+            this.costo += MatriceDistanze.getInstanza().getDistanza(n.getId(), nodi.get(nodi.size() - 1).getId());
         }
-
         this.nodi.add(n);
     }
 
-    public void chiudiRotta(){
-        Nodo deposito=nodi.get(0);
-        this.costo+=MatriceDistanze.getInstanza().getDistanza(deposito.getId(),nodi.get(nodi.size() - 1).getId());
+    public void chiudiRotta() {
+        Nodo deposito = nodi.get(0);
+        this.costo += MatriceDistanze.getInstanza().getDistanza(deposito.getId(), nodi.get(nodi.size() - 1).getId());
         this.nodi.add(deposito);
     }
 
@@ -41,13 +39,8 @@ public class Rotta extends MainRotta {
         return nodi;
     }
 
-    public Rotta rimuoviNodo(){
-        //eliminare il nodo dalla rotta aggiornando il costo e aumentare nuovamente la capacità del veicolo
-        return this;
-    }
-
-    public void stampaRotta(ArrayList<Nodo> n){
-        for(Nodo a:n){
+    public void stampaRotta(ArrayList<Nodo> n) {
+        for (Nodo a : n) {
             System.out.print(a.getId() + ",");
         }
     }
