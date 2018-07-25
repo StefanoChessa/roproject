8623e54bbbc624e9b18d6df3878a53a15b048fae
Alessio <alessio@manai.tk>
2018-07-25 18:32:35 +0200

    Stampa risultati beta

---
+++ src/ro/project/PrintResults.java
@@ -0,0 +1,20 @@
+package ro.project;
+
+public class PrintResults {
+
+    double costoX;
+    double costoP;
+
+    public PrintResults(double costoX, double costoP){
+
+        this.costoX = costoX;
+        this.costoP = costoP;
+
+    }
+
+    void printOnFile(String filename){
+
+
+
+    }
+}
